99ccc8c72187abe55b09786c3e78c124a3f534ad
JaspherJ <jazoswald@gmail.com>
2017-10-08 20:44:47 -0400

    Updated upto React Life Cycle Hooks

--- src/app/index.js
+++ src/app/index.js
@@ -1,22 +1,58 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import {render} from "react-dom";
 
-import { Header } from "./components/Header";
-import { Home } from "./components/Home";
+import {Header} from "./components/Header";
+import {Home} from "./components/Home";
 
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            homeLink: "Home",
+            homeMounted:true
+        };
+    }
+
+    onGreet() {
+        alert("Hello!");
+    }
+
+    onChangeLinkName(newName) {
+        this.setState({
+            homeLink: newName
+        });
+    }
+    onChangeHomeMounted(){
+        this.setState({
+
+            homeMounted: !this.state.homeMounted
+        });
+    }
 
     render() {
+        let homeCmp = "";
+        if(this.state.homeMounted){
+            homeCmp = (
+                <Home
+                    name={"Max"}
+                    initialAge={27}
+                    greet={this.onGreet}
+                    changeLink={this.onChangeLinkName.bind(this)}
+                    initialLinkName={this.state.homeLink}
+                />
+
+            );
+        }
         return (
             <div className="container">
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Header/>
+                        <Header homeLink={this.state.homeLink}/>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Home/>
+                        {homeCmp}
                     </div>
                 </div>
             </div>
@@ -24,4 +60,4 @@ class App extends React.Component {
     }
 }
 
-render(<App />, window.document.getElementById('app'));
+render(<App/>, window.document.getElementById("app"));
